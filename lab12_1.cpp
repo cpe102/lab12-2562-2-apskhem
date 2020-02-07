ecbbeef345037f21711710f2bcb1c41d574083fa
Apisit Ritreungroj <apisit_lon@hotmail.com>
2020-02-07 16:55:17 +0700

    completed

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 using namespace std;
 
-void stat(const double[],int,double []);
+void stat(const double [], int, double []);
 
 int main()
 {
@@ -18,3 +18,35 @@ int main()
 }
 
 //Write definition of stat() here 
+void stat(const double dataIn[], int n, double dataOut[]) {
+    // average process
+    dataOut[0] = 0;
+    for (short i = 0; i < n; i++) {
+        dataOut[0] += dataIn[i];
+    }
+    dataOut[0] = dataOut[0] / n;
+    
+    // S.D. process
+    double sumPow = 0;
+    for (short i = 0; i < n; i++) {
+        sumPow += pow(dataIn[i] - dataOut[0], 2);
+    }
+    
+    dataOut[1] = pow(sumPow / n, 0.5);
+    
+    // max process
+    dataOut[2] = dataIn[0];
+    for (short i = 0; i < n; i++) {
+        if (dataOut[2] < dataIn[i]) {
+            dataOut[2] = dataIn[i];
+        }
+    }
+    
+    // min process
+    dataOut[3] = dataIn[0];
+    for (short i = 0; i < n; i++) {
+        if (dataOut[3] > dataIn[i]) {
+            dataOut[3] = dataIn[i];
+        }
+    }
+}
--- lab12_2.cpp
+++ lab12_2.cpp
@@ -2,8 +2,8 @@
 #include <cmath>
 using namespace std;
 
-const int N = 30;
-const int M = 70;
+const int N = 32;
+const int M = 72;
 
 void updateImage(bool [][M],int,int,int);
 
@@ -11,15 +11,46 @@ void showImage(const bool [][M]);
 
 int main()
 {
-    bool image[N][M] = {};
+    bool image[N][M];
     int s,x,y;
-    do{
+    
+    // init image
+    for (int row = 0; row < N; row++) {
+        for (int col = 0; col < M; col++) {
+            image[row][col] = false;
+        }
+    }
+    
+    do {
         showImage(image);
-        cout << "Input your brush size and location:";
+        cout << "Input your brush size and location: ";
         cin >> s >> x >> y;
         updateImage(image,s,x,y);
-    }while(s != 0 && x != 0 && y != 0 );
-             return 0;    
+    } while(s != 0 && x != 0 && y != 0 );
+    
+    return 0;
 }
 
 // Write definition of updateImage() and showImage() here
+void updateImage(bool img[][M], int bushSize, int x, int y) {
+    for (int row = 0; row < N; row++) {
+        for (int col = 0; col < M; col++) {
+            int a = pow(row-x, 2);
+            int b = pow(col-y, 2);
+            if (sqrt(a + b) <= bushSize-1)
+                img[row + 1][col + 1] = true;
+        }
+    }
+}
+
+void showImage(const bool img[][M]) {
+    for (int row = 0; row < N; row++) {
+        for (int col = 0; col < M; col++) {
+            if (row == 0 || row == N-1) cout << '-';
+            else if (col == 0 || col == M-1) cout << '|';
+            else if (img[row][col]) cout << '*';
+            else cout << ' ';
+        }
+        cout << endl;
+    }
+}
